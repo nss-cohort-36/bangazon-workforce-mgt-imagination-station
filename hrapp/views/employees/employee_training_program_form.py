19bddb7ca13d51efa2d1e61ac66f70b6577fa73f
Lauren Riddle <lari7132@gmail.com>
2020-02-12 15:33:19 -0600

    got training programs list to filter

--- hrapp/views/employees/employee_training_program_form.py
+++ hrapp/views/employees/employee_training_program_form.py
@@ -22,9 +22,21 @@ def get_programs():
 
         return db_cursor.fetchall()
 
+def create_program(cursor, row):
+    _row = sqlite3.Row(cursor, row)
+    programslist = list()
+
+    program = TrainingProgram()
+    program.id = _row["id"]
+    program.title = _row["title"]
+    program.start_date = _row["start_date"]
+    program.end_date = _row["end_date"]
+    programslist.append(program)
+    return programslist
+
 def get_relationships(employee_id):
     with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = model_factory(EmployeeTrainingProgram)
+        conn.row_factory = create_program
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
@@ -32,24 +44,32 @@ def get_relationships(employee_id):
             t.id,
             t.title,
             t.start_date,
-            t.end_date,
-            et.training_program_id,
-            et.employee_id
+            t.end_date
             from hrapp_employeetrainingprogram et 
             join hrapp_trainingprogram t on et.training_program_id = t.id
             where et.employee_id = ?
-        """, (employee_id))
+        """, (employee_id,))
     
         return db_cursor.fetchall()
 
 def employee_training_program_form(request, employee_id):
     if request.method == 'GET':
+        program_list = list()
         employeeId = employee_id
         programs = get_programs()
         relationships = get_relationships(employee_id)
+        programIds_list = list()
+        for relationship in relationships:
+            programIds_list.append(relationship[0].id)
+        for program in programs:
+            if program.id not in programIds_list:
+                program_list.append(program)
+                print("yes")
+            else:
+                print("hey")
         template = 'employees/employee_training_program_form.html'
         context = {
-            'all_programs': programs,
+            'all_programs': program_list,
             'employeeId': employeeId,
             'relationships': relationships
         }
