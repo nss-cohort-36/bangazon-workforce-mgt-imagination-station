43e05a44dba757acafd3cac2e4c977c77def15d4
Ryan Crowley <ryancrowleycode@gmail.com>
2020-02-12 16:07:00 -0600

    computer details will now show who is assigned the computer

--- hrapp/views/computers/computer_details.py
+++ hrapp/views/computers/computer_details.py
@@ -3,10 +3,11 @@ from django.shortcuts import render, reverse, redirect
 from django.contrib.auth.decorators import login_required
 from hrapp.models import Computer, model_factory
 from ..connection import Connection
+from django.contrib import messages
 
 def get_computer(computer_id):
     with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = model_factory(Computer)
+        conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
@@ -15,12 +16,36 @@ def get_computer(computer_id):
             c.make,
             c.model,
             c.purchase_date,
-            c.decommission_date
-        FROM hrapp_computer c
+            c.decommission_date,
+            e.first_name,
+            e.last_name            
+        FROM 
+            hrapp_computer c
+        LEFT JOIN 
+            hrapp_employeecomputer ec
+        ON
+            ec.computer_id = c.id
+        LEFT JOIN 
+            hrapp_employee e
+        ON 
+            ec.employee_id = e.id
         WHERE c.id = ?
         """, (computer_id,))
 
-        return db_cursor.fetchone()
+        data_set = db_cursor.fetchone()
+        computer = Computer()
+        computer.id = data_set['id']
+        computer.make = data_set['make']
+        computer.model = data_set['model']
+        computer.purchase_date = data_set['purchase_date']
+        computer.decommission_date = data_set['decommission_date']
+        computer.first_name = data_set['first_name']
+        computer.last_name = data_set['last_name']
+
+        return computer
+
+
+
 
 @login_required
 def computer_details(request, computer_id):
