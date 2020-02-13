dd435887dfbf54d8c5de6e85bf3df60f01c26a31
Michelle Johnson <bob1234girl@gmail.com>
2020-02-13 10:39:10 -0600

    working on displaying unassigned computers

--- hrapp/views/employees/form.py
+++ hrapp/views/employees/form.py
@@ -2,6 +2,8 @@ import sqlite3
 from django.shortcuts import render, reverse, redirect
 from django.contrib.auth.decorators import login_required
 from hrapp.models.department import Department
+from hrapp.models.computer import Computer
+from hrapp.models.employee_computer import EmployeeComputer
 from hrapp.models import model_factory
 from ..connection import Connection
 from hrapp.views.employees.employee_details import get_employee
@@ -19,6 +21,36 @@ def get_departments():
         """)
 
         return db_cursor.fetchall()
+
+def get_computer():
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = model_factory(Computer)
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        select 
+            c.id,
+            c.make,
+            c.model
+            from hrapp_computer c
+            """)
+
+        return db_cursor.fetchone()
+
+def get_assigned_computer():
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = model_factory(EmployeeComputer)
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        select 
+            c.id,
+            c.make,
+            c.model,
+            ec.unassigned_date
+            from hrapp_computer c 
+            left join hrapp_employeecomputer ec on c.id = ec.computer_id
+            where ec.unassigned_date = ? """)
+
+        return db_cursor.fetchone()
 
 @login_required
 def employee_form(request):
